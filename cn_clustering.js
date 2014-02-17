478afd02e6931d1715f5621e7f4c64e2d31c82b6
Scott Robinson <srobin@mit.edu>
2014-02-17 04:58:13 -0500

    replaced naive nearest neighbor qith quadtree radius search

--- cn_clustering.js
+++ cn_clustering.js
@@ -2,29 +2,19 @@
 
 function cn(points, clusterCount){ //clusterCount is the number of clusters to restrict the plot to
     var closestPoint = []
+    var plotsize = -1
 
-    for (var i = 0; i < points.length; i++){
-        var x = points[i][0]
-        var y = points[i][1]
+    for(var i = 0; i < points.length; i++){ //need plotsize for quadtree
+        if(points[i][0] > plotsize)
+            plotsize = points[i][0]
+        if(points[i][1] > plotsize)
+            plotsize = points[i][1]
+    }
 
-        var leastDist = -1 //invalid default
-        var point = -1
+    var qtree = new quadtree(points,0,0,plotsize+1,plotsize+1) //+1 to avoid edge cases
 
-        for(var j = 0; j < points.length; j++){
-            if(i == j)
-                continue
-
-            var x1 = points[j][0]
-            var y1 = points[j][1]
-
-            var dist = Math.sqrt(Math.pow((x1-x),2) + Math.pow((y1-y),2))
-
-            if(dist < leastDist || leastDist == -1){
-                leastDist = dist
-                point = j
-            }
-        }
-        closestPoint.push(point)
+    for(var i = 0; i < points.length; i++){
+        closestPoint.push(qtree.findNearestPoint(points[i])[2])
     }
 
     console.log('Finished finding closest neighbor')
@@ -75,6 +65,184 @@ function cn(points, clusterCount){ //clusterCount is the number of clusters to r
     }
 
     return cn_return
+}
+
+function quadtree(points,x,y,x1,y1){
+    for(var i = 0; i < points.length; i++){ //for nn position
+        points[i].push(i)
+    }
+
+    this.points = points
+    this.x = x
+    this.y = y
+    this.x1 = x1
+    this.y1 = y1
+    this.tree = {}
+
+    qt_recurse(this.points,this.tree,this.x,this.y,this.x1,this.y1)
+
+    function qt_recurse(points,context,x,y,x1,y1){
+        context.x = x
+        context.y = y
+        context.x1 = x1
+        context.y1 = y1
+
+        if(points.length == 1){
+            context.points = points
+            return
+        }
+
+        var equal = true
+        for(var i = 1; i < points.length; i++){
+            if(String(points[i][0]) !== String(points[0][0]) || String(points[1][1]) !== String(points[i][1]) ){
+                equal = false
+                break
+            }
+        }
+
+        if(equal){
+            context.points = points
+            return
+        }
+
+        context[0] = {}
+        context[1] = {}
+        context[2] = {}
+        context[3] = {}
+
+        var points0 = []
+        var points1 = []
+        var points2 = []
+        var points3 = []
+
+        for(var i = 0; i < points.length; i++){
+            var point = points[i]
+
+            if(point[0] < (x+x1)/2){
+                if(point[1] < (y+y1)/2){
+                    points0.push(point)
+                }
+                else{
+                    points2.push(point)
+                }
+            }
+            else{
+                if(point[1] < (y+y1)/2){
+                    points1.push(point)
+                }
+                else{
+                    points3.push(point)
+                }
+            }
+        }
+
+        if(points0.length > 0){
+            qt_recurse(points0,context[0],x,y,(x+x1)/2,(y+y1)/2)
+        }
+        if(points1.length > 0){
+            qt_recurse(points1,context[1],(x+x1)/2,y,x1,(y+y1)/2)
+        }
+        if(points2.length > 0){
+            qt_recurse(points2,context[2],x,(y+y1)/2,(x+x1)/2,y1)
+        }
+        if(points3.length > 0){
+            qt_recurse(points3,context[3],(x+x1)/2,(y+y1)/2,x1,y1)
+        }
+    }
+}
+
+quadtree.prototype.findNearestPoint = function(point,radius){
+    var r = radius || Math.max(this.x1,this.y1)
+    var closest = []
+    var closestDist = -1
+    
+    nn(this.tree,point)
+    return closest
+
+    function nn(ctx,pt){
+        if(ctx.points){
+            for(var i = 0; i<ctx.points.length; i++){
+                var p1 = ctx.points[i]
+                if(String(p1) == String(pt))
+                    continue
+                var dist = Math.sqrt(Math.pow((p1[0]-pt[0]),2) + Math.pow((p1[1] - pt[1]),2))
+                if(dist < closestDist || closestDist == -1){
+                    closestDist = dist
+                    closest = p1
+                }
+            }
+        }
+
+        if(0 in ctx){ //therefore 0,1,2,3
+            if('x' in ctx[0]){ //therefore y,x1,y1
+                if(inRadius(ctx[0].x,ctx[0].y,ctx[0].x1,ctx[0].y1,pt,r)){
+                    nn(ctx[0],pt)
+                }
+            }
+            if('x' in ctx[1]){
+                if(inRadius(ctx[1].x,ctx[1].y,ctx[1].x1,ctx[1].y1,pt,r)){
+                    nn(ctx[1],pt)
+                }
+            }
+            if('x' in ctx[2]){
+                if(inRadius(ctx[2].x,ctx[2].y,ctx[2].x1,ctx[2].y1,pt,r)){
+                    nn(ctx[2],pt)
+                }
+            }
+            if('x' in ctx[3]){
+                if(inRadius(ctx[3].x,ctx[3].y,ctx[3].x1,ctx[3].y1,pt,r)){
+                    nn(ctx[3],pt)
+                }
+            }
+        }
+
+        function inRadius(x,y,x1,y1,pt,r){ //will be 9
+            var isIn = true
+            var distance //from point to region
+
+            if(pt[0] < x && pt[1] < y){
+                distance = Math.sqrt(Math.pow((x-pt[0]),2) + Math.pow((y-pt[1]),2))
+                //to x,y
+            }
+            if(pt[0] > x && pt[0] < x1){
+                if(pt[1] < y){
+                    distance = y-pt[1]
+                }
+                if(pt[1] > y && pt[1] < y1){
+                    return isIn
+                    //isIn = true (center of square)
+                }
+                if(pt[1] > y1){
+                    distance = pt[1] - y1
+                }
+            }
+            if(pt[0] > x1 && pt[1] < y){
+                distance = Math.sqrt(Math.pow((x1-pt[0]),2) + Math.pow((y-pt[1]),2))
+                //to x1,y
+            }
+            if(pt[1] > y && pt[1] < y1){
+                if(pt[0] > x1){
+                    distance = pt[0] - x1
+                }
+                if(pt[0] < x){
+                    distance = x - pt[0]
+                }
+            }
+            if(pt[0] > x1 && pt[1] > y1){
+                distance = Math.sqrt(Math.pow((x1-pt[0]),2) + Math.pow((y1-pt[1]),2))
+                //to x1,y1
+            }
+            if(pt[0] < x && pt[1] > y1){
+                distance = Math.sqrt(Math.pow((x-pt[0]),2) + Math.pow((y1-pt[1]),2))
+                //to x,y1
+            }
+
+            if(distance > r)
+                isIn = false
+
+            return isIn
+        }
+    }
 }
 
 //example call:
